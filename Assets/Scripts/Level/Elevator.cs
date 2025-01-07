6821f8dadb1037635f7bbc180d60ac0972cb1cdd
Kyle Kotovsky <kkotovsky@mail.bradley.edu>
2025-01-06 22:05:40 -0600

    Elevator is done

--- Assets/Scripts/Level/Elevator.cs
+++ Assets/Scripts/Level/Elevator.cs
@@ -1,8 +1,15 @@
+using System;
 using UnityEngine;
 
 public class Elevator : MonoBehaviour
 {
-    [SerializeField] private Animator _elevatorAnimator;
+    private Animator _elevatorAnimator;
+
+    private void Awake()
+    {
+        _elevatorAnimator = GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
             _elevatorAnimator.SetTrigger("Activate");
